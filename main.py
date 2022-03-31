ea04a18a1422f775d30613a767c98579cdef42b8
Mohammad Umar Javed <80000522+MohammadUmarJaved@users.noreply.github.com>
2022-03-31 10:53:20 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
 from colorama import Fore, Style,Back
 import random
+import pyfiglet
 import kms
 clo = [Fore.RED, Fore.GREEN, Fore.YELLOW, Fore.BLUE, Fore.MAGENTA, Fore.CYAN, Fore.WHITE]
 
-print(f"""
-███╗   ██╗████████╗██╗     
-████╗  ██║╚══██╔══╝██║     
-██╔██╗ ██║   ██║   ██║     
-██║╚██╗██║   ██║   ██║     
-██║ ╚████║   ██║   ███████╗
-╚═╝  ╚═══╝   ╚═╝   ╚══════╝
+print(f""" {Fore.LIGHTCYAN_EX}{pyfiglet.figlet_format("NTL",font="epic",width=100)}
 {random.choice(clo)}Network Scanner  v1.0  {Style.RESET_ALL}
 {Fore.YELLOW}Made By {Fore.CYAN}@MuhammadUmarFarooq{Style.RESET_ALL}                 
 """)
 
 while True:
     print("Enter the IP address or domain name: ", end="")
-    inp = input("⫸⫸⫸  ")
+    inp = input(">>>  ")
     f_cms = kms.find_cms(inp)
     for key,value in f_cms.items():
         print(f"{Fore.GREEN}[+]{Style.RESET_ALL} {key} {Fore.GREEN}----> {Fore.YELLOW}{value}{Style.RESET_ALL}")
